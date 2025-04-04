dba841e46d2b5fb0d30f55acfa3fa16e29ec1e89
Mohamed Danyane <mohameddanyane@gmail.com>
2025-04-04 19:16:13 +0000

    api with docker setup

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Client } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+// Connect to PostgreSQL
+const client = new Client({
+  host: 'db',  // Use 'db' because that's the service name of PostgreSQL in docker-compose.yml
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+client.connect();
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await client.query('SELECT id, name FROM users');
+    res.json(result.rows); 
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+INSERT INTO users (name) VALUES
+('Mohamed'),
+('younes'),
+('ahmed');
